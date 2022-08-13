520de4cd81108577690869b01b9e675d508affda
Alvin Sanchez <loquilloll@gmail.com>
2022-08-13 07:44:09 -0400

    Tapping term 225

--- keyboards/keebio/iris/keymaps/loquillo/config.h
+++ keyboards/keebio/iris/keymaps/loquillo/config.h
@@ -20,4 +20,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 150
+#define TAPPING_TERM 225
